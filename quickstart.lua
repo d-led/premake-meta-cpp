11d09ba5f6811bcbd916a3b6e373433755374508
Dmitry Ledentsov <dmlled@yahoo.com>
2014-07-13 18:46:13 +0200

    add simple packaging

--- quickstart.lua
+++ quickstart.lua
@@ -1,6 +1,7 @@
 cfg = assert( require 'premake.config' )
 actions = assert( require 'premake.actions' )
 util = assert( require 'premake.util' )
+patterns = assert( require 'patterns' )
 
 _G.make_solution = actions.make_solution
 _G.make_console_app = actions.make_console_app
@@ -8,3 +9,4 @@ _G.make_static_lib = actions.make_static_lib
 _G.make_shared_lib = actions.make_shared_lib
 _G.run_target_after_build = actions.run_target_after_build
 _G.make_cpp11 = actions.make_cpp11
+_G.make_package = patterns.make_package

0b82ea9bd0775ce80ca43610ab1641a9aea2bf1b
Dmitry Ledentsov <dmlled@yahoo.com>
2014-07-13 18:58:09 +0200

    fix command

--- patterns.lua
+++ patterns.lua
@@ -6,7 +6,7 @@ patterns.make_package = function(files_to_package,filename_without_extension)
 	        description = 'prepare a binary release',
 	        execute     = function ()
 	        	local command = 'tar -cvpzf '..
-	        					filename_without_extension
+	        					filename_without_extension ..
 	        					'.tar.gz ' ..
 	        					table.concat( files_to_package, ' ')
 	            os.execute(command)

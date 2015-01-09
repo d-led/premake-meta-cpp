e58f9d1c6209b5a40b595d118e48104f8c0f19e1
Dmitry Ledentsov <dmlled@yahoo.com>
2015-01-09 11:09:10 +0100

    update test

--- test/premake4.lua
+++ test/premake4.lua
@@ -3,6 +3,7 @@ include '..'
 config.location_pattern = [[CustomBuild/%o/%v/%t]]
 
 make_solution 'test'
+platforms { "native","x32", "x64" }
 make_console_app('test', { 'test.cpp' }) 
 use_standard('c++11') 
 run_target_after_build()
